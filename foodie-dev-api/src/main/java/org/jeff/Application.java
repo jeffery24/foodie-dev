341c6069bf1692776f4c1ac49b4be7afd28263e9
zsj <Arapekah@gmail.com>
2022-02-27 02:27:52 +0800

    修复设置cookie域问题: An invalid domain [] was specified for this

--- foodie-dev-api/src/main/java/org/jeff/Application.java
+++ foodie-dev-api/src/main/java/org/jeff/Application.java
@@ -1,18 +1,39 @@
 package org.jeff;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "org.jeff.mapper")
 // 扫描所有包以及所有相关的组件包, 如果扫描org.jeff.mapper会导致请求Path404 -- 暂时未知
 @ComponentScan(basePackages = {"org.jeff", "org.n3r.idworker"})
+// 开启定时任务
+@EnableScheduling
+// 开启使用redis作为spring session
+//@EnableRedisHttpSession
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    /*
+        对于 Tomcat 8.5后的设置cookie的新规范, .zsj.zone这样以点开头的域报错解决
+        An invalid domain [] was specified for this cookie问题解决
+     */
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (factory) -> factory.addContextCustomizers(
+                (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
+    }
+
 }
