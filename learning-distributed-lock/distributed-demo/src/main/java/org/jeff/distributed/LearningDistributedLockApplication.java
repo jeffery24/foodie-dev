5797e4c54da4c74aa3d448b99f095e3c87dcb4d2
zsj <Arapekah@gmail.com>
2022-05-30 14:48:18 +0800

    基于Redisson实现分布式锁

--- learning-distributed-lock/distributed-demo/src/main/java/org/jeff/distributed/LearningDistributedLockApplication.java
+++ learning-distributed-lock/distributed-demo/src/main/java/org/jeff/distributed/LearningDistributedLockApplication.java
@@ -3,11 +3,10 @@ package org.jeff.distributed;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("org.jeff.distributed.dao")
-@EnableScheduling
+//@EnableScheduling
 public class LearningDistributedLockApplication {
 
     public static void main(String[] args) {
